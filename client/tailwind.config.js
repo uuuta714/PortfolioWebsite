aa8b78e9714cd10b7b01b7a04c2b97ad2a2065fb
uuuta714 <horiyuuu@gmail.com>
2024-09-30 09:37:15 +1000

    Initial UI design for top page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,36 @@
+const { addDynamicIconSelectors } = require('@iconify/tailwind');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ["Bebas Neue"],
+
+        body: ["Manrope"],
+      }
+    },
   },
   plugins: [
     require('daisyui'),
+    addDynamicIconSelectors(),
   ],
+  daisyui: {
+    themes: [
+      'lemonade',
+      {'dark': {
+        "primary": "#D3E97A",
+        "primary-content": "#000000",
+        "secondary": "#f8fbe9",
+        "secondary-content": "#000000",
+        "base-100": "#0A0A0A",
+        "base-200": "#1A1A1A",
+        "base-300": "#333333",
+      }},
+    ]
+  },
 }
 
